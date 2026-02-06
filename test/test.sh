8c034e6612b0e4278efd7ce8cc1657d952e92680
Mika Steyer <mikasteyer9@gmail.com>
2026-02-06 18:01:35 +0100

    fix: correct path

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 # POST Request with File Upload
-curl -X POST "http://localhost:8000/jobs" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./native.pdf;type=application/pdf"
+curl -X POST "http://localhost:8000/jobs" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./test/native.pdf;type=application/pdf"
 
-curl -X POST "http://localhost:8000/jobs" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./scan_default.pdf;type=application/pdf"
+curl -X POST "http://localhost:8000/jobs" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./test/scan_default.pdf;type=application/pdf"
 
-curl -X POST "http://localhost:8000/jobs" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./scan_bad.pdf;type=application/pdf"
+curl -X POST "http://localhost:8000/jobs" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./test/scan_bad.pdf;type=application/pdf"
